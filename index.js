da3cc4170b7ce0df623f62595a3ec4b3bd94db9d
Daniel Taraba <daniel.taraba@konzepthaus-ws.de>
2025-04-11 10:45:11 +0200

    Update title for the update custom object form

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/', async (_req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    res.render('updates', { title: 'Update Series Object Form' });
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
